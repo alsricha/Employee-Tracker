3838ba9ea1871f7bf12589a538fafd77d469c525
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-13 09:12:19 -0600

    Create Table for Employees for database

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,31 @@
-DROP DATABASE IF EXISTS cms;
 
-CREATE database cms;
+DROP DATABASE IF EXISTS employee_db; 
 
-USE cms;
+CREATE database employee_db;
 
-CREATE TABLE department (
-    id INTEGER NOT NULL auto_increment PRIMARY KEY,
-    name VARCHAR (30)
+USE employee_db;
+
+CREATE TABLE department(
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(50)
 );
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INTEGER,
+    FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREGIN KEY (manager_id) REFERENCES role(id)
+);
+
+
