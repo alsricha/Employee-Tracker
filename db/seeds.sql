f7c2c0fad1fb0c197c7b7bbe5ed5de70a452e527
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-09-13 09:18:45 -0600

    Added syntax to insert department types (i.e. HR, Sales, IT, etc.)

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE employee_db;
+
+INSERT into department (name) VALUES ("Sales");
+INSERT into department (name) VALUES ("IT");
+INSERT into department (name) VALUES ("HR");
+INSERT into department (name) VALUES ("Alex");
+
+
