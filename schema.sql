fc29dc5cbf0c6c57fbeff27088eb4410b38b8291
Alex Skudrovskis <64692833+alsricha@users.noreply.github.com>
2020-09-13 11:02:42 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -19,13 +19,13 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INTEGER,
-    manager_id INTEGER,
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-    FOREGIN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
-
 
